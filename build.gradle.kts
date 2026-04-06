59b20f58e1c1be1ae848755114f71e63a2dff75c
Lomig Mégard <dev@lomig.ch>
2026-04-06 22:01:14 +0200

    Configure IDE versions for plugin verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,12 @@ intellijPlatform {
             Initial version
         """.trimIndent()
     }
+
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
 }
 
 tasks {
